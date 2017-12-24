e4f9deceb88f132be335f94d7041d9e86c8c5cea
Jarred Parr <jparr721@gmail.com>
2017-12-24 16:14:59 -0500

    wow I fucked up

--- prov.py
+++ prov.py
@@ -27,7 +27,7 @@ if euid != 0:
     # the next line replaces the currently-running process with the sudo
     os.execlpe('sudo', *args)
 
-print 'Running. Your euid is', euid
+print("Running. Your euid is"+euid)
 
 print("Provisionator, the only provisioning tool you need!")
 print("1 - Ubuntu Server")
@@ -59,7 +59,7 @@ def serverInstall():
         tar = tarfile.open(gofile, "r:gz")
         tar.extractall("/usr/local/go")
         tar.close()
-    elif (gofile.endswith("tar"))
+    elif (gofile.endswith("tar")):
         tar = tarfile.open(gofile, "r:")
         tar.extractall("/usr/local/go")
         tar.close()
@@ -67,7 +67,28 @@ def serverInstall():
     os.system("export GOROOT=/usr/local/go")
     os.system("mkdir $HOME/server")
     os.system("export GOPATH=$HOME/server")
-    os.system("export PATH=$GOPATH/bin:$GOROOT/bin:$PATH)
+    os.system("export PATH=$GOPATH/bin:$GOROOT/bin:$PATH")
 
-if __name__ == "__main__":
-    beginInstall(distro[0])
+    print("Verifying install...")
+    os.system("go version")
+    os.system("sleep 2")
+    os.system("go env")
+    print("If there are any defects, please remove the install from /usr/local/go and try again manually...")
+    os.system("sleep 2")
+
+    # Grab some repos if needed.
+    os.system("python3 brogrammer.py")
+
+    # Pick up some systems
+    print("Need a web server? A database? We got that too!")
+    print("======================OPTIONS=======================")
+    print("Web Servers:")
+    print("1 - Apache\t\t2 - Nginx")
+    print("\n")
+    print("Databases:")
+    print("3 - MySql\t\t4 - Postgres\t\t5 - MongoDB (Not Recommended for prod)")
+    options = list()
+    options = input("Select options: (Comma separated ex. 1,2,3)")
+    numbers = options.split(',')
+    for opt in options:
+        os.system(packageManager+opt)
