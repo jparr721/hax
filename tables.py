1a8ed0fce65dbf0a51c834f29d2824d9f951f5be
Jarred <jparr721@gmail.com>
2018-01-05 14:04:56 -0500

    Added general table layout

--- tables.py
+++ tables.py
@@ -30,33 +30,18 @@ def createTables():
         )
         """,
         """
-        CREATE TABLE posts (
+        CREATE TABLE sample (
             post_id SERIAL PRIMARY KEY,
             title VARCHAR(140) NOT NULL,
             price money,
-            description text,
-            user_name VARCHAR(100) NOT NULL,
-            user_id integer NOT NULL,
-            category VARCHAR(150) NOT NULL,
-            date_added timestamp NOT NULL,
-            date_modified timestamp NOT NULL,
-            is_live boolean NOT NULL
+            description text
         )
         """,
-        """
-        CREATE TABLE schools (
-            school_id SERIAL PRIMARY KEY,
-            school_name CHAR(256) NOT NULL,
-            school_address_1 VARCHAR(256) NOT NULL,
-            school_address_2 VARCHAR(256) NOT NULL,
-            school_state CHAR(2) NOT NULL
-        )
-        """
     )
     conn = None
     try:
         # Set up credentials (change password on serverjjj)
-        conn = psycopg2.connect(database='unified', user='unified', host='localhost', password='dbpass')
+        conn = psycopg2.connect(database='dbname', user='dbuser', host='localhost', password='dbpass')
 
         # Connect to postgres
         cur = conn.cursor()
